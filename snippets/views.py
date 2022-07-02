430ba7fdfe2c9ecc1106b169b9ca19343c702026
skkim <skkim@mediquitous.com>
2022-07-02 23:37:13 +0900

    Add snippet list and detail views

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 
-# Create your views here.
+from .models import Snippet
+
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        data = list(snippets.values())
+        return JsonResponse(data, safe=False)
+
+    return JsonResponse({"error": f"Not Supported Method `{request.method}`"})
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        data = model_to_dict(snippet)
+        return JsonResponse(data, safe=False)
+
+    return JsonResponse({"error": f"Not Supported Method `{request.method}`"})
